a969b602c2a5791930d6fa199b67af2e09f53db8
nobumitsu_masugata <masugata1995@gmail.com>
2024-03-16 20:11:30 +0900

    feat: SubHeadingコンポーネントの作成

---
+++ src/components/UI/Atoms/SubHeading/SubHeading.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css'
+
+export const subHeading = style({
+  fontSize: 24,
+  fontWeight: 'bold',
+  letterSpacing: 2,
+})
---
+++ src/components/UI/Atoms/SubHeading/SubHeading.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { SubHeading } from './SubHeading'
+
+type ComponentType = typeof SubHeading
+type Story = StoryObj<ComponentType>
+
+export default {
+  component: SubHeading,
+  args: {
+    text: '記事検索',
+  },
+} as Meta<ComponentType>
+
+export const Primary: Story = {}
