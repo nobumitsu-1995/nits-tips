7530bf7e49a8161a61a7b0475ffad8e722c10e4e
nobumitsu_masugata <masugata1995@gmail.com>
2024-03-16 16:24:04 +0900

    feat: グローバルCSSの設定

--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -1,1 +1,8 @@
+import { globalStyle } from '@vanilla-extract/css'
 import './reset.css'
+import { vars } from './theme.css'
+
+globalStyle(':root', {
+  fontFamily: vars.font.bizUD,
+  color: vars.color.darkGray,
+})
