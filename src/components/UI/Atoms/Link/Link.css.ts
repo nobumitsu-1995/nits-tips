a6e3cca8f68dce64f71ae4205eddfd994753e8eb
nobumitsu_masugata <masugata1995@gmail.com>
2024-03-16 18:42:51 +0900

    feat: Linkコンポーネントの調整

--- src/components/UI/Atoms/Link/Link.css.ts
+++ src/components/UI/Atoms/Link/Link.css.ts
@@ -1,5 +1,11 @@
 import { style } from '@vanilla-extract/css'
+import { vars } from '../../../../styles/theme.css'
+import { hover } from '../../../../styles/global.css'
 
-export const link = style({
-  color: '#000',
-})
+export const link = style([
+  hover,
+  {
+    color: vars.color.link,
+    fontWeight: 'bold',
+  },
+])
--- src/components/UI/Atoms/Link/Link.tsx
+++ src/components/UI/Atoms/Link/Link.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as styles from './Link.css'
+import { ICON_TYPE, Icon } from '../Icon'
 
 type Props = {
   /** 表示するテキスト */
@@ -18,5 +19,6 @@ export const Link: React.FC<Props> = ({ text, href, isTagetBlank = false }) => (
     className={styles.link}
   >
     {text}
+    {isTagetBlank && <Icon type={ICON_TYPE.open_in_new} />}
   </a>
 )
