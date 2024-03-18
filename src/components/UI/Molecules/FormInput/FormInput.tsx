40333ca70e06f22a8f6265ff5b40cd3e9137c17a
nobumitsu_masugata <masugata1995@gmail.com>
2024-03-18 23:46:59 +0900

    fix: FormInputにtextareaモードを追加

--- src/components/UI/Molecules/FormInput/FormInput.tsx
+++ src/components/UI/Molecules/FormInput/FormInput.tsx
@@ -1,15 +1,19 @@
 import React, { useId, type ComponentProps } from 'react'
 import { INPUT_TYPE, Input } from '@Atoms/Input'
+import { TextBox } from '@Atoms/TextBox'
 import * as styles from './FormInput.css'
 
-type Props = Omit<ComponentProps<typeof Input>, 'isError'> & {
+type Props = Omit<ComponentProps<typeof Input>, 'isError' | 'onChange'> & {
   label: string
   errorText?: string
+  isTextArea?: boolean
+  onChange: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>
 }
 
 export const FormInput: React.FC<Props> = ({
   label,
   errorText = '',
+  isTextArea = false,
   name,
   value,
   onChange,
@@ -21,22 +25,40 @@ export const FormInput: React.FC<Props> = ({
   const id = useId()
 
   return (
-    <label htmlFor={name} className={styles.formInput}>
-      <span className={`${styles.label} ${isDisabled ? styles.disabled : ''}`}>
+    <label
+      htmlFor={name}
+      className={`${styles.formInput} ${isTextArea ? styles.isTextArea : ''}`}
+    >
+      <span
+        className={`${styles.label} ${isDisabled ? styles.disabled : ''} ${isTextArea ? styles.isTextAreaLabel : ''}`}
+      >
         {label}
         {isRequired && <span className={styles.required}>*必須</span>}
       </span>
-      <Input
-        name={name}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        type={type}
-        isDisabled={isDisabled}
-        isRequired={isRequired}
-        isError={!!errorText}
-        a11yId={errorText ? id : undefined}
-      />
+      {isTextArea ? (
+        <TextBox
+          name={name}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          isDisabled={isDisabled}
+          isRequired={isRequired}
+          isError={!!errorText}
+          a11yId={errorText ? id : undefined}
+        />
+      ) : (
+        <Input
+          name={name}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          type={type}
+          isDisabled={isDisabled}
+          isRequired={isRequired}
+          isError={!!errorText}
+          a11yId={errorText ? id : undefined}
+        />
+      )}
       {!!errorText && (
         <span aria-live="polite" id={id} className={styles.error}>
           {errorText}
