25397f8f6e5007714b3fbe9eb02ad5d6e5c79b89
nobumitsu_masugata <masugata1995@gmail.com>
2024-03-21 23:39:58 +0900

    feat: microCMSのレスポンスに合わせて修正

---
+++ src/components/Organisms/Detail/Article/Content/Content.css.ts
@@ -0,0 +1,103 @@
+import { globalStyle, style } from '@vanilla-extract/css'
+import { vars } from '../../../../../styles/theme.css'
+
+export const content = style({})
+
+globalStyle(`${content} img`, {
+  maxWidth: '100%',
+  height: 'auto',
+})
+
+globalStyle(`${content} p`, {
+  margin: '16px 0',
+  lineHeight: 2,
+})
+
+globalStyle(`${content} blockquote`, {
+  padding: '8px 24px',
+  marginLeft: 28,
+  borderLeft: `5px solid ${vars.color.lightGray}`,
+  color: vars.color.gray,
+})
+
+globalStyle(`${content} blockquote`, {
+  padding: '8px 24px',
+  marginLeft: 28,
+  borderLeft: `5px solid ${vars.color.lightGray}`,
+  color: vars.color.gray,
+})
+
+globalStyle(`${content} blockquote p`, {
+  margin: 0,
+})
+
+globalStyle(`${content} hr`, {
+  borderTop: `1px dashed ${vars.color.lightGray}`,
+  margin: '36px 0',
+})
+
+globalStyle(`${content} a`, {
+  color: vars.color.link,
+  fontWeight: 'bold',
+})
+
+globalStyle(`${content} a:hover`, {
+  opacity: 0.8,
+  transitionDuration: '0.3s',
+  transitionProperty: 'opacity',
+})
+
+globalStyle(`${content} ul`, {
+  margin: '12px 0',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 12,
+  listStyle: 'square',
+  paddingLeft: 20,
+})
+
+globalStyle(`${content} ol`, {
+  margin: '12px 0',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 12,
+  listStyle: 'decimal',
+  paddingLeft: 20,
+})
+
+globalStyle(`${content} code`, {
+  padding: '2px 6px',
+  backgroundColor: '#2b2b2b',
+  borderRadius: 2,
+  color: '#f8f8f2',
+})
+
+globalStyle(`${content} table`, {
+  tableLayout: 'auto',
+  border: `2px solid ${vars.color.lightGray}`,
+  borderCollapse: 'collapse',
+})
+
+globalStyle(`${content} table p`, {
+  margin: 0,
+})
+
+globalStyle(`${content} th`, {
+  padding: '4px 12px',
+  backgroundColor: vars.color.background,
+  textAlign: 'center',
+  border: `1px solid ${vars.color.lightGray}`,
+})
+
+globalStyle(`${content} tr`, {
+  border: `1px solid ${vars.color.lightGray}`,
+})
+
+globalStyle(`${content} tr&:nth-child(2n)`, {
+  backgroundColor: vars.color.lightGray,
+})
+
+globalStyle(`${content} td`, {
+  padding: '4px 12px',
+  border: `1px solid ${vars.color.lightGray}`,
+})
--- src/components/Organisms/Detail/Article/Content/Content.tsx
+++ src/components/Organisms/Detail/Article/Content/Content.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { MarkdownRenderer } from '@md-parser/react'
-import { components } from './Components'
+import React, { useEffect } from 'react'
+import hljs from 'highlight.js/lib/core'
+import 'highlight.js/styles/a11y-dark.min.css'
+import { setupHljs } from '@/lib/helpers/setupHljs'
+import * as styles from './Content.css'
 
 type Props = {
   /** 記事の内容(markdown) */
   content: string
 }
 
-export const Content: React.FC<Props> = ({ content }) => (
-  <MarkdownRenderer components={components}>{content}</MarkdownRenderer>
-)
+setupHljs()
+
+export const Content: React.FC<Props> = ({ content }) => {
+  useEffect(() => {
+    hljs.initHighlighting()
+  })
+
+  return (
+    // eslint-disable-next-line react/no-danger
+    <div
+      className={styles.content}
+      dangerouslySetInnerHTML={{ __html: content }}
+    />
+  )
+}
