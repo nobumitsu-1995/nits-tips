31dbacebc5676ef7245214d4c27daf86f2a17efa
nobumitsu_masugata <masugata1995@gmail.com>
2024-03-25 22:29:44 +0900

    add: npx astro add vercelの実行

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from 'astro/config'
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import react from '@astrojs/react';
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   trailingSlash: 'always',
   integrations: [react()],
   vite: {
-    plugins: [vanillaExtractPlugin()],
+    plugins: [vanillaExtractPlugin()]
   },
-})
+  output: "server",
+  adapter: vercel()
+});
