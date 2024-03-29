cfb1ebf5aa5c45d9af6f527002753e41c5022dc3
nobumitsu_masugata <masugata1995@gmail.com>
2024-03-30 00:08:11 +0900

    feat: about画面のSP対応

--- src/components/Entrypoints/about/About/About.css.ts
+++ src/components/Entrypoints/about/About/About.css.ts
@@ -1,6 +1,12 @@
 import { style } from '@vanilla-extract/css'
+import { breakpoint } from '../../../../styles/theme.css'
 
 export const container = style({
   margin: '0 auto',
   width: 792,
+  '@media': {
+    [`${breakpoint.medium}`]: {
+      width: '100%',
+    },
+  },
 })
--- src/styles/pages/about.css.ts
+++ src/styles/pages/about.css.ts
@@ -1,17 +1,27 @@
 import { style } from '@vanilla-extract/css'
-import { vars } from '../theme.css'
+import { breakpoint, vars } from '../theme.css'
 
 export const container = style({
-  padding: '80px 0',
+  padding: '80px 12px',
   backgroundColor: vars.color.background,
 })
 
 export const cardContainer = style({
   margin: '0 auto',
   width: 994,
+  '@media': {
+    [`${breakpoint.medium}`]: {
+      width: '100%',
+    },
+  },
 })
 
 export const contactForm = style({
   margin: '0 auto',
   width: 792,
+  '@media': {
+    [`${breakpoint.medium}`]: {
+      width: '100%',
+    },
+  },
 })
