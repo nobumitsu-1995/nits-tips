167316d0e5abbcdd2e33614b63de29565d1e704f
nobumitsu_masugata <masugata1995@gmail.com>
2024-04-06 21:23:47 +0900

    feat: SearchCardコンポーネントからSearchForm部分はPropsとして埋め込むように修正

--- src/components/Organisms/search/SearchCard/SearchCard.tsx
+++ src/components/Organisms/search/SearchCard/SearchCard.tsx
@@ -1,42 +1,20 @@
-import React, { useId, useState } from 'react'
+import React, { useId, type PropsWithChildren } from 'react'
 import * as styles from './SearchCard.css'
 import { Card } from '@/components/UI/Atoms/Card'
 import { SubHeading } from '@/components/UI/Atoms/SubHeading'
 import { Border } from '@/components/UI/Atoms/Border'
 import { Link } from '@/components/UI/Atoms/Link'
-import { Icon } from '@/components/UI/Atoms/Icon'
 
-export const SearchCard: React.FC = () => {
+type Props = PropsWithChildren
+
+export const SearchCard: React.FC<Props> = ({ children }) => {
   const a11yId = useId()
-  const [searchWord, setSearchWord] = useState('')
-
-  const handleChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchWord(e.target.value)
-  }
 
   return (
     <Card padding="24px 36px">
       <SubHeading text="記事検索" id={a11yId} />
       <Border margin="20px 0" />
-      <form
-        aria-labelledby={a11yId}
-        className={styles.form}
-        action="/search/result/"
-        method="get"
-      >
-        <input
-          type="search"
-          placeholder="検索"
-          aria-labelledby={a11yId}
-          name="q"
-          value={searchWord}
-          onChange={handleChangeSearch}
-          className={styles.input}
-        />
-        <button type="submit" aria-label="検索" className={styles.button}>
-          <Icon type="search" size={24} />
-        </button>
-      </form>
+      {children}
       <div className={styles.linkContainer}>
         <Link
           className={styles.link}
