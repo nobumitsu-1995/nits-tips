73221ef0b6b38966f9948bfe5cc9bfae915b92dd
nobumitsu_masugata <masugata1995@gmail.com>
2024-04-06 22:03:25 +0900

    feat: searchカードをclient:loadで記事詳細ページに呼び出す

--- src/components/Entrypoints/detail/SideBar/Presenter.tsx
+++ src/components/Entrypoints/detail/SideBar/Presenter.tsx
@@ -1,7 +1,6 @@
 import React, { type ComponentProps, type PropsWithChildren } from 'react'
 import { ArticleIndex } from '@Organisms/Detail/ArticleIndex'
 import * as styles from './SideBar.css'
-import { Card } from '@/components/UI/Atoms/Card'
 
 type Props = PropsWithChildren<{
   articleIndex: ComponentProps<typeof ArticleIndex>['articleIndex']
@@ -11,9 +10,7 @@ export const Presenter: React.FC<Props> = ({ articleIndex, children }) => (
   <div className={styles.stickyContainer}>
     <div className={styles.sideBar}>
       <ArticleIndex articleIndex={articleIndex} />
-      <Card>
-        <div className={styles.shareContainer}>{children}</div>
-      </Card>
+      {children}
     </div>
   </div>
 )
--- src/styles/pages/detail.css.ts
+++ src/styles/pages/detail.css.ts
@@ -44,3 +44,8 @@ export const asideContainer = style({
     },
   },
 })
+
+export const shareContainer = style({
+  display: 'flex',
+  justifyContent: 'center',
+})
