c8c7a97bde521b6980361787abcecc24310cf961
nobumitsu_masugata <masugata1995@gmail.com>
2024-04-07 15:57:43 +0900

    fix: SP時の関連記事のスタイルを修正

--- src/components/Organisms/Detail/RelatedArticle/RelatedArticle.css.ts
+++ src/components/Organisms/Detail/RelatedArticle/RelatedArticle.css.ts
@@ -1,8 +1,17 @@
 import { style } from '@vanilla-extract/css'
+import { breakpoint } from '../../../../styles/theme.css'
 
 export const list = style({
   marginTop: 24,
   display: 'grid',
   gridTemplateColumns: '1fr 1fr',
   gap: '24px 20px',
+  '@media': {
+    [`${breakpoint.medium}`]: {
+      gridTemplateColumns: '1fr 1fr',
+    },
+    [`${breakpoint.small}`]: {
+      gridTemplateColumns: '1fr',
+    },
+  },
 })
