4c6691d472e51c3de86e82c2fac76bab600932b8
nobumitsu_masugata <masugata1995@gmail.com>
2024-04-10 00:00:27 +0900

    feat: SP向けにスタイルを調整

--- src/components/Organisms/search/FilterSearch/CategorySelector/CategorySelector.css.ts
+++ src/components/Organisms/search/FilterSearch/CategorySelector/CategorySelector.css.ts
@@ -3,4 +3,5 @@ import { style } from '@vanilla-extract/css'
 export const categorySelector = style({
   display: 'flex',
   gap: 16,
+  flexWrap: 'wrap',
 })
--- src/components/Organisms/search/FilterSearch/TagSelector/TagSelector.css.ts
+++ src/components/Organisms/search/FilterSearch/TagSelector/TagSelector.css.ts
@@ -3,4 +3,5 @@ import { style } from '@vanilla-extract/css'
 export const tagSelector = style({
   display: 'flex',
   gap: 16,
+  flexWrap: 'wrap',
 })
