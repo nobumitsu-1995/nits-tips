559a54c3cfcb75519f652be2622eec59251291c0
nobumitsu_masugata <masugata1995@gmail.com>
2024-04-09 21:26:14 +0900

    feat: ワード検索フィールドの作成

---
+++ src/components/Organisms/search/TextSearch/TextSearch.css.ts
@@ -0,0 +1,11 @@
+import { style } from '@vanilla-extract/css'
+
+export const textSearch = style({})
+
+export const formContainer = style({
+  marginTop: 24,
+  maxWidth: 570,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 12,
+})
---
+++ src/components/Organisms/search/TextSearch/TextSearch.tsx
@@ -0,0 +1,26 @@
+import React, { useId, useState } from 'react'
+import { Heading } from '@Atoms/Heading'
+import * as styles from './TextSearch.css'
+import { SearchForm } from '../SearchForm'
+import {
+  SearchTypeSelector,
+  SEARCH_TYPE,
+  type SearchType,
+} from './SearchTypeSelector'
+
+export const TextSearch: React.FC = () => {
+  const [searchType, setSearchType] = useState<SearchType>(SEARCH_TYPE.all)
+  const a11yId = useId()
+  return (
+    <section className={styles.textSearch}>
+      <Heading id={a11yId} text="ワード検索" />
+      <div className={styles.formContainer}>
+        <SearchTypeSelector
+          searchType={searchType}
+          setSearchType={setSearchType}
+        />
+        <SearchForm id={a11yId} searchType={searchType} />
+      </div>
+    </section>
+  )
+}
