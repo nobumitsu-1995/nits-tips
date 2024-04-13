3a239000cad82b8798fe190b0529870c1708c4a0
nobumitsu_masugata <masugata1995@gmail.com>
2024-04-13 16:03:19 +0900

    feat: 切り分けたコンポーネントで TagFilterを修正

--- src/components/Organisms/search/SearchConditions/TagFilter/TagFilter.css.ts
+++ src/components/Organisms/search/SearchConditions/TagFilter/TagFilter.css.ts
@@ -1,45 +1,7 @@
 import { style } from '@vanilla-extract/css'
-import { vars } from '../../../../../styles/theme.css'
 
 export const tagFilter = style({
   display: 'flex',
   alignItems: 'flex-start',
-  gap: 16,
-})
-
-export const title = style({
-  display: 'flex',
-  alignItems: 'center',
-  flexShrink: 0,
-  transform: 'translateY(2px)',
-  fontWeight: 'bold',
-})
-
-export const buttonContainer = style({
-  display: 'flex',
-  flexWrap: 'wrap',
-  gap: 4,
-})
-
-export const tagContainer = style({
-  display: 'flex',
-  alignItems: 'center',
-  gap: 2,
-  padding: '2px 8px',
-  backgroundColor: vars.color.white,
-  border: `2px solid ${vars.color.lightGray}`,
-  borderRadius: 16,
-})
-
-export const button = style({
-  padding: 0,
-  width: 24,
-  height: 24,
-  border: 'none',
-  borderRadius: '50%',
-  backgroundColor: vars.color.white,
-  cursor: 'pointer',
-  ':hover': {
-    backgroundColor: '#eee',
-  },
+  gap: 12,
 })
--- src/components/Organisms/search/SearchConditions/TagFilter/TagFilter.tsx
+++ src/components/Organisms/search/SearchConditions/TagFilter/TagFilter.tsx
@@ -1,43 +1,23 @@
 import React from 'react'
 import * as styles from './TagFilter.css'
-import type { MicroCMS } from '@/types/microCMS'
-import { Icon, ICON_TYPE } from '@/components/UI/Atoms/Icon'
-import { Tag } from '@/components/UI/Molecules/Tag'
-import { vars } from '@/styles/theme.css'
+import { TagTitle } from './TagTitle'
+import { CurrentTags } from './CurrentTags'
+
+export type TagType = {
+  id: string
+  label: string
+  src?: string
+}
 
 type Props = {
-  tags: MicroCMS['tag'][]
+  selectedTags: TagType[]
+  tags: TagType[]
   onClick: (id: string) => void
 }
 
-export const TagFilter: React.FC<Props> = ({ tags, onClick }) => (
+export const TagFilter: React.FC<Props> = ({ selectedTags, tags, onClick }) => (
   <div className={styles.tagFilter}>
-    <p className={styles.title}>
-      <Icon type={ICON_TYPE.sell} size={24} />
-      タグ
-    </p>
-    <div className={styles.buttonContainer}>
-      {tags.map((tag) => (
-        <div key={tag.id} className={styles.tagContainer}>
-          <button
-            type="button"
-            onClick={() => onClick(tag.id)}
-            aria-label={`${tag.label}を絞り込み条件から削除`}
-            className={styles.button}
-          >
-            <Icon
-              type={ICON_TYPE.cancel}
-              size={24}
-              color={vars.color.lightGray}
-            />
-          </button>
-          <Tag
-            src={tag.icon?.url}
-            label={tag.label}
-            href={`/search/result/?tag=${tag.id}`}
-          />
-        </div>
-      ))}
-    </div>
+    <TagTitle tags={tags} />
+    <CurrentTags selectedTags={selectedTags} onClick={onClick} />
   </div>
 )
