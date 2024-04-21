62b9d6b7e91718f69512e0b4a9d6d0d49979d636
nobumitsu_masugata <masugata1995@gmail.com>
2024-04-21 14:06:01 +0900

    fix: storybookに書いたテストをvitest側に移行(FormInputコンポーネント)

--- src/components/UI/Molecules/FormInput/FormInput.stories.tsx
+++ src/components/UI/Molecules/FormInput/FormInput.stories.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
-import { fn, userEvent, within, expect } from '@storybook/test'
+import { fn } from '@storybook/test'
 import { FormInput } from './FormInput'
 
 type ComponentType = typeof FormInput
@@ -23,38 +23,16 @@ export default {
   ],
 } as Meta<ComponentType>
 
-export const Default: Story = {
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement)
-    const input = canvas.getByRole('textbox')
-    await userEvent.type(input, 'example')
-    expect(input).toHaveDisplayValue('example')
-  },
-}
+export const Default: Story = {}
 
 export const IsDisabled: Story = {
   args: {
     isDisabled: true,
-  },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement)
-    const input = canvas.getByRole('textbox')
-    await userEvent.type(input, 'example')
-    expect(input).not.toHaveDisplayValue('example')
-    expect(input).toHaveAttribute('disabled')
   },
 }
 
 export const IsError: Story = {
   args: {
     errorText: '※この値は必須です',
-  },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement)
-    const input = canvas.getByRole('textbox')
-    expect(input).toHaveAttribute('aria-invalid', 'true')
-    expect(input).toHaveAccessibleDescription('※この値は必須です')
-    const error = canvas.getByText('※この値は必須です')
-    expect(error).toHaveAttribute('aria-live', 'polite')
   },
 }
