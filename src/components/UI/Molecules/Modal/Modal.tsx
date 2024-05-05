3141e35ac81a201a0b939fd5061fcd6ffbb84417
nobumitsu_masugata <masugata1995@gmail.com>
2024-05-05 23:56:08 +0900

    fix: Cardは必要ないので削除

--- src/components/UI/Molecules/Modal/Modal.tsx
+++ src/components/UI/Molecules/Modal/Modal.tsx
@@ -1,5 +1,4 @@
 import React, { type PropsWithChildren } from 'react'
-import { Card } from '@Atoms/Card'
 import * as styles from './Modal.css'
 
 type Props = PropsWithChildren<{
@@ -7,20 +6,13 @@ type Props = PropsWithChildren<{
   ariaControls: string
   /** 開閉状態 */
   isOpen: boolean
-  /** Cardのpadding */
-  padding?: string
 }>
 
-export const Modal: React.FC<Props> = ({
-  ariaControls,
-  isOpen,
-  padding,
-  children,
-}) => (
+export const Modal: React.FC<Props> = ({ ariaControls, isOpen, children }) => (
   <div
     className={`${styles.modal} ${isOpen ? styles.isOpen : styles.isHidden}`}
     aria-controls={ariaControls}
   >
-    <Card padding={padding}>{children}</Card>
+    {children}
   </div>
 )
