063f9e12d3b28de29c16244f864185e49d649a3a
nobumitsu_masugata <masugata1995@gmail.com>
2024-05-07 23:56:16 +0900

    fix: ローディング用のcssを追加

--- src/components/UI/Atoms/TextLoading/TextLoading.css.ts
+++ src/components/UI/Atoms/TextLoading/TextLoading.css.ts
@@ -1,8 +1,15 @@
-import { style } from '@vanilla-extract/css'
-import { vars } from '../../../../styles/theme.css'
+import { keyframes, style } from '@vanilla-extract/css'
+
+const shine = keyframes({
+  to: {
+    backgroundPositionX: '-200%',
+  },
+})
 
 export const textLoading = style({
   width: '100%',
-  backgroundColor: vars.color.lightGray,
-  borderRadius: 8,
+  background: 'linear-gradient(110deg, #ececec 8%, #f5f5f5 18%, #ececec 33%)',
+  backgroundSize: '200% 100%',
+  borderRadius: 4,
+  animation: `0.8s ${shine} linear infinite`,
 })
