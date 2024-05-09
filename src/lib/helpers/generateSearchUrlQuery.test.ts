3c093b78bf4aba4d2a94283d0c829e9e1e8cd512
nobumitsu_masugata <masugata1995@gmail.com>
2024-05-09 21:50:31 +0900

    feat: URLクエリ作成用の関数を作成

---
+++ src/lib/helpers/generateSearchUrlQuery.test.ts
@@ -0,0 +1,26 @@
+import { SEARCH_TYPE } from '@/components/Organisms/search/TextSearch'
+import { generateSearchUrlQuery } from './generateSearchUrlQuery'
+
+const FILTER_SEARCH_PARAMS = {
+  tags: ['testTag1', 'testTag2'],
+  category: 'testCategory',
+}
+
+const WORD_SEARCH_PARAMS = {
+  searchType: SEARCH_TYPE.all,
+  searchWord: 'testSearchWord',
+}
+
+test('絞り込み検索の引数が渡された時、絞込検索用のURLクエリを作成する', () => {
+  expect(generateSearchUrlQuery(FILTER_SEARCH_PARAMS)).toMatchSnapshot()
+})
+
+test('絞り込み検索の引数でtagsが空配列で渡された時、絞込検索用のURLクエリを作成する', () => {
+  expect(
+    generateSearchUrlQuery({ ...FILTER_SEARCH_PARAMS, tags: [] }),
+  ).toMatchSnapshot()
+})
+
+test('ワード検索の引数が渡された時、ワード検索用のURLクエリを作成する', () => {
+  expect(generateSearchUrlQuery(WORD_SEARCH_PARAMS)).toMatchSnapshot()
+})
