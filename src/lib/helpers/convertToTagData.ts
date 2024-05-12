1e78c6da7424f7d9f800f9fc8bc05dad0530a5b9
nobumitsu_masugata <masugata1995@gmail.com>
2024-05-12 14:17:25 +0900

    feat: convertToTagDataを修正

--- src/lib/helpers/convertToTagData.ts
+++ src/lib/helpers/convertToTagData.ts
@@ -1,9 +1,11 @@
 import type { MicroCMS } from '@/types/microCMS'
 
-export const convertToTagData = (tags: MicroCMS['tag'][]) =>
+export const convertToTagData = (
+  tags: (MicroCMS['tag'] | MicroCMS['category'])[],
+) =>
   tags.map((data) => ({
     id: data.id,
     label: data.label,
-    href: `/search/result/?tag=${data.id}`,
-    src: data.icon?.url ?? '',
+    href: `/search/result/?${'icon' in data ? 'tag' : 'category'}=${data.id}`,
+    src: 'icon' in data ? data.icon.url : '',
   }))
