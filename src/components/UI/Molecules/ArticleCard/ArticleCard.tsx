631d5a70078f671c4a68b7dd820aa0cce4623ad2
nobumitsu_masugata <masugata1995@gmail.com>
2024-05-18 14:42:59 +0900

    fix: ArticleCardの重なりでバグがあったので修正

--- src/components/UI/Molecules/ArticleCard/ArticleCard.tsx
+++ src/components/UI/Molecules/ArticleCard/ArticleCard.tsx
@@ -16,14 +16,18 @@ export const ArticleCard: React.FC<Props> = ({
   updatedAt,
   thumbnail,
 }) => (
-  <Card padding="24px 20px">
-    <PublishedAt publishedAt={publishedAt} updatedAt={updatedAt} />
+  <Card padding="24px 0px">
+    <div className={styles.container}>
+      <PublishedAt publishedAt={publishedAt} updatedAt={updatedAt} />
+    </div>
     <h3>
       <a href={`/detail/${id}/`}>
         <img className={styles.img} src={thumbnail} alt={title} />
       </a>
     </h3>
-    <p className={styles.text}>{description}</p>
-    <TagList tagData={tagData} />
+    <div className={styles.container}>
+      <p className={styles.text}>{description}</p>
+      <TagList tagData={tagData} />
+    </div>
   </Card>
 )
