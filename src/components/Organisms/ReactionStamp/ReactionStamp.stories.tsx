27d5fcfb2d029639f98811e60def13cc3c21a962
nobumitsu_masugata <masugata1995@gmail.com>
2024-06-27 21:35:15 +0900

    feat: useReactionStampをUIに組み込む

--- src/components/Organisms/ReactionStamp/ReactionStamp.stories.tsx
+++ src/components/Organisms/ReactionStamp/ReactionStamp.stories.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
+import { fn } from '@storybook/test'
 import { ReactionStamp } from './ReactionStamp'
+import { REACTION_STAMPS } from './model'
 
 type ComponentType = typeof ReactionStamp
 type Story = StoryObj<ComponentType>
 
 export default {
   component: ReactionStamp,
-  args: {},
+  args: {
+    isLoading: false,
+    reactionStampSummary: [
+      { stamp: REACTION_STAMPS[0], count: 12, isChecked: false },
+      { stamp: REACTION_STAMPS[5], count: 24, isChecked: true },
+    ],
+    reactedStamp: [1, 2, 3],
+    handlePostStamp: fn(),
+    handleDeleteStamp: fn(),
+    articleId: 'articleId',
+  },
   decorators: [
     (Component) => (
       <div style={{ paddingTop: 40 }}>
