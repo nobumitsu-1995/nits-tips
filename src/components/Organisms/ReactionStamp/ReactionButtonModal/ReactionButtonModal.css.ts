37c0c888f1c46357078af126da0a978031b790b3
nobumitsu_masugata <masugata1995@gmail.com>
2024-07-15 12:11:25 +0900

    feat: ReactionButtonのスタイルを調整

--- src/components/Organisms/ReactionStamp/ReactionButtonModal/ReactionButtonModal.css.ts
+++ src/components/Organisms/ReactionStamp/ReactionButtonModal/ReactionButtonModal.css.ts
@@ -1,9 +1,11 @@
 import { style } from '@vanilla-extract/css'
+import { vars } from '../../../../styles/theme.css'
 
 export const reactionButtonModal = style({
   display: 'inline-flex',
   gap: 4,
   padding: '2px 4px',
   borderRadius: 18,
+  backgroundColor: vars.color.white,
   boxShadow: '2px 4px 12px 0px rgba(0, 0, 0, 0.2)',
 })
