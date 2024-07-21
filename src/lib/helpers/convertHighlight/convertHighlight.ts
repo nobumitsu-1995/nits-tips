6234c651788c473678cf4409564a528d0eedb6ba
nobumitsu_masugata <masugata1995@gmail.com>
2024-07-21 21:48:06 +0900

    feat: シンタックスハイライトをつける関数を作成

---
+++ src/lib/helpers/convertHighlight/convertHighlight.ts
@@ -0,0 +1,16 @@
+import hljs from 'highlight.js'
+import { load } from 'cheerio'
+import 'highlight.js/styles/a11y-dark.min.css'
+
+export const convertHighlight = (content: string) => {
+  const $ = load(content)
+
+  $('pre').each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text())
+
+    $(elm).html(result.value)
+    $(elm).addClass(elm.attribs.class)
+  })
+
+  return $.html()
+}
