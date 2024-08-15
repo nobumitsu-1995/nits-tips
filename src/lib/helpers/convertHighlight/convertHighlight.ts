92dca7ec441c4452f61548d2f54bc23907389a1c
nobumitsu_masugata <masugata1995@gmail.com>
2024-08-15 12:53:51 +0900

    feat: preタグを操作してfileNameを抽出、付与するように修正

--- src/lib/helpers/convertHighlight/convertHighlight.ts
+++ src/lib/helpers/convertHighlight/convertHighlight.ts
@@ -11,6 +11,15 @@ export const convertHighlight = (content: string) => {
     $(elm).html(result.value)
     $(elm).addClass(elm.attribs.class)
     $('<pre>').replaceWith('<pre tabindex="0">')
+
+    const parentElement = elm.parent
+    if (!parentElement || !('attribs' in parentElement)) return
+
+    const fileName = parentElement.attribs['data-filename']
+    if (!fileName) return
+
+    $(elm).addClass('with_file_name')
+    $(elm).append(`<span class="file_name">${fileName}</span>`)
   })
 
   return $.html()
