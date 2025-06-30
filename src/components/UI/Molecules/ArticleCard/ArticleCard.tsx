ec2e7f734acc976acd5cc19cd37709eb11859693
nobumitsu_masugata <masugata1995@gmail.com>
2025-06-30 22:54:54 +0900

    feat: popupコンポーネントに組み込む

--- src/components/UI/Molecules/ArticleCard/ArticleCard.tsx
+++ src/components/UI/Molecules/ArticleCard/ArticleCard.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Card } from '@Atoms/Card'
 import * as styles from './ArticleCard.css'
 import { TagList } from '../TagList'
 import { PublishedAt } from '../../Atoms/PublishedAt'
 import type { ArticleCardData } from '@/lib/interfaces/Article'
+import { Popup } from '@/components/UI/Atoms/Popup'
 
 type Props = ArticleCardData
 
@@ -15,19 +16,45 @@ export const ArticleCard: React.FC<Props> = ({
   publishedAt,
   updatedAt,
   thumbnail,
-}) => (
-  <Card padding="24px 0px">
-    <div className={styles.container}>
-      <PublishedAt publishedAt={publishedAt} updatedAt={updatedAt} />
-    </div>
-    <h3>
-      <a href={`/detail/${id}/`}>
-        <img className={styles.img} src={thumbnail} alt={title} />
-      </a>
-    </h3>
-    <div className={styles.container}>
-      <p className={styles.text}>{description}</p>
-      <TagList tagData={tagData} />
-    </div>
-  </Card>
-)
+}) => {
+  const [isPopupShown, setIsPopupShown] = useState(false)
+  const [isOverflowing, setIsOverflowing] = useState(false)
+  const descriptionRef = useRef<HTMLParagraphElement>(null)
+
+  useEffect(() => {
+    if (descriptionRef.current) {
+      if (
+        descriptionRef.current.scrollHeight >
+        descriptionRef.current.clientHeight
+      ) {
+        setIsOverflowing(true)
+      }
+    }
+  }, [description])
+
+  return (
+    <Card padding="24px 0px">
+      <div className={styles.container}>
+        <PublishedAt publishedAt={publishedAt} updatedAt={updatedAt} />
+      </div>
+      <h3>
+        <a href={`/detail/${id}/`}>
+          <img className={styles.img} src={thumbnail} alt={title} />
+        </a>
+      </h3>
+      <div className={styles.container}>
+        <div
+          onMouseEnter={() => setIsPopupShown(true)}
+          onMouseLeave={() => setIsPopupShown(false)}
+          className={styles.descriptionContainer}
+        >
+          <p className={styles.text} ref={descriptionRef}>
+            {description}
+          </p>
+          <Popup isOpen={isPopupShown && isOverflowing}>{description}</Popup>
+        </div>
+        <TagList tagData={tagData} />
+      </div>
+    </Card>
+  )
+}
